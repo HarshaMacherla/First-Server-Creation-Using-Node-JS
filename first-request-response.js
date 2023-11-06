e6fc1c87a07c6617b469ea5fb46896c53e44566d
HarshaMacherla <macherlaharsha997@gmail.com>
2023-11-06 17:25:50 +0530

    Written logic to conditionally serve a response based on the custom urls

--- first-request-response.js
+++ first-request-response.js
@@ -1,7 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("My name is Harshavardhan Macherla.");
+  res.write("<html>");
+  res.write("<body>");
+  if (req.url === "/home") {
+    res.write("<h1>Welcome home</h1>");
+  } else if (req.url === "/about") {
+    res.write("<h1>Welcome to About us</h1>");
+  } else if (req.url === "/node") {
+    res.write("<h1>Welcome to my Node Js project</h1>");
+  } else {
+    res.write("<h1>Request unknown!</h1>");
+  }
+  res.write("</body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(4000);
